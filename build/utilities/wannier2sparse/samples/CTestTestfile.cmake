7d6cfeb04fcf9dfd6d4064e87db1da1c4ba996ed
José H. Garcia <josehugo.garcia@icn2.cat>
2020-07-25 14:32:19 +0200

    deleted build

--- build/utilities/wannier2sparse/samples/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /data/jgarcia/codes/linqt/utilities/wannier2sparse/samples
-# Build directory: /data/jgarcia/codes/linqt/build/utilities/wannier2sparse/samples
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
